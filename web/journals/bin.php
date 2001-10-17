88b98602e0bd2257490166d011ab102d5cb44f07
swinney <swinney>
2001-10-17 23:30:36 +0000

    noice.

--- web/journals/bin.php
+++ web/journals/bin.php
@@ -1,11 +1,35 @@
 <?
+##################################
+# $Id$
 # experiments in binary
 # funny how little it takes.
-include "global.inc";
+echo "<H1>experiments in binary</H1>";
+echo "<P><a href=\"". $PHP_SELF ."s\">source file is available</a></P>";
 
-for ($i=0; $i<=14;$i++) {
-$res = 1<<$i;
-echo "$res is ". bin($res)."<BR>";
+##################################
+# bin($dec) function for converting
+# decimals to binary.  really just
+# to view them in a browser, that is all.
+# there is no need to do this otherwise.
+
+function bin($dec){
+  $bin = sprintf("%8b", $dec);
+  $bin = ereg_replace(" ","0",$bin);
+  return $bin;
 }
 
+
+echo "<PRE>";
+for ($i=0; $i<=7;$i++) {
+$res = 1<<$i;
+echo bin($res)." is $res<BR>";
+}
+
+$numA=14;
+$numB=12;
+$res = $numA & $numB;
+echo "\n\n$numA & $numB is $res<BR>";
+echo "\nA: ". bin($numA);
+echo "\nB: ". bin($numB);
+echo "</PRE>";
 ?>
