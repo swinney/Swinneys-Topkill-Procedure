0908b5e4694baa0414e2859dbc03ae521831519c
swinney <swinney>
2001-10-24 20:07:30 +0000

    bin files are for my binary project.  i modified insert_thought to deal with images named JPG not just jpg.

--- web/journals/bin.php
+++ web/journals/bin.php
@@ -3,33 +3,8 @@
 # $Id$
 # experiments in binary
 # funny how little it takes.
-echo "<H1>experiments in binary</H1>";
-echo "<P><a href=\"". $PHP_SELF ."s\">source file is available</a></P>";
-
-##################################
-# bin($dec) function for converting
-# decimals to binary.  really just
-# to view them in a browser, that is all.
-# there is no need to do this otherwise.
-
-function bin($dec){
-  $bin = sprintf("%8b", $dec);
-  $bin = ereg_replace(" ","0",$bin);
-  return $bin;
-}
-
-
-echo "<PRE>";
-for ($i=0; $i<=7;$i++) {
-$res = 1<<$i;
-echo bin($res)." is $res<BR>";
-}
-
-$numA=14;
-$numB=12;
-$res = $numA & $numB;
-echo "\n\n$numA & $numB is $res<BR>";
-echo "\nA: ". bin($numA);
-echo "\nB: ". bin($numB);
-echo "</PRE>";
+  $title = "Php & Binary for Categorical Systems";
+  $rollcall = "0";
+  $into_template = "bin.inc";
+  include "template.inc";
 ?>
---
+++ web/journals/bin_converter.php
@@ -0,0 +1,28 @@
+<?
+////////////////////////////////
+// $Id$
+// converting binary form
+require_once('./global.inc');
+if (isset($num)) {
+  $bin=bin($bit,$num);
+  echo "<P><TT>base10: $num</TT><BR>\n";
+  echo    "<TT>binary: $bin</TT></P>\n";
+}
+?>
+<FORM NAME="bin_converter" ACTION="<? echo $PHP_SELF; ?>" METHOD="POST">
+<TABLE><TR><TD>
+<INPUT TYPE="radio" NAME="bit" VALUE=8>8 bit
+</TD><TD>
+<INPUT TYPE="radio" NAME="bit" VALUE=16>16 bit
+</TD><TD>
+<INPUT TYPE="radio" NAME="bit" VALUE=24>24 bit
+</TD><TD>
+<INPUT TYPE="radio" NAME="bit" VALUE=32>32 bit
+</TD><TD>
+<INPUT TYPE="radio" NAME="bit" VALUE=64>64 bit
+</TD></TR>
+</TABLE>
+
+<INPUT TYPE="TEXT" NAME=num COLS=30>
+<INPUT TYPE="SUBMIT" NAME="submit" VALUE="convert">
+</FORM>
