6b79c77b24c03ff5e12d4fd5ecb95c2f3758b13f
swinney <swinney>
2002-02-01 18:51:12 +0000

    initial source.  this should only have additions, the info should not change.

---
+++ web/journals/local_conf.php
@@ -0,0 +1,11 @@
+<?
+define("URLBASE", "http://your.site.base.com"); // no trailing slash!
+define("DIRBASE", "/path/to/your/site/dir");
+define("IMGBASE", "/path/to/your/img/upload/dir");
+define("IMGURL", URLBASE . "/journals/images");
+define("CSS_URL", URLBASE . "/journals/main.css");
+define("DB_HOST", "localhost");
+define("DB_USER", "your_username");
+define("DB_PASSWD", "your_password");
+define("DB_NAME", "your_database");
+?>
