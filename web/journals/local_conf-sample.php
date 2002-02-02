1282e3d258712fee1f198e0b55f90ec2c891bfb4
swinney <swinney>
2002-02-02 01:48:16 +0000

    ok ok ok, sample config file.

---
+++ web/journals/local_conf-sample.php
@@ -0,0 +1,14 @@
+<?php
+// -*- Mode: PHP; indent-tabs-mode: nil; =*-
+////////////////////////////////////////////////////////
+// $Id$
+define("URLBASE", "http://you.site.com"); // no trailing slash!
+define("DIRBASE", "/home/username/html_dir");
+define("IMGBASE", "/home/username/html_dir/images");
+define("IMGURL", URLBASE ."/journals/images");
+define("CSS_URL", URLBASE ."/journals/main.css");
+define("DB_HOST", "localhost");
+define("DB_USER", "username");
+define("DB_PASSWD", "password");
+define("DB_NAME", "database");
+?>
