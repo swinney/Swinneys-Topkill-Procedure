31b42a1a89df15a5fa72ffaf04c065dac8fa5c87
bcm <bcm>
2002-03-08 01:13:03 +0000

    add INCBASE, and HOME which is useful for shorthanding the others

--- web/journals/local_conf-sample.php
+++ web/journals/local_conf-sample.php
@@ -3,11 +3,13 @@
 ////////////////////////////////////////////////////////
 // $Id$
 define("URLBASE", "http://you.site.com"); // no trailing slash!
-define("DIRBASE", "/home/username/html_dir");
-define("IMGBASE", "/home/username/html_dir/images");
 define("IMGURL", URLBASE ."/journals/images");
 define("CSS_URL", URLBASE ."/journals/main.css");
-define("LIBDIR", "/home/username/lib_dir");
+define("HOME", "/home/username/swinsite-2");
+define("DIRBASE", HOME . "/web/journals");
+define("IMGBASE", DIRBASE . "/images");
+define("INCDIR", DIRBASE . "/templates");
+define("LIBDIR", HOME . "/lib");
 define("DB_HOST", "localhost");
 define("DB_USER", "username");
 define("DB_PASSWD", "password");
